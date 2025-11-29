ff9b6e852eee042b1f92644fa0ae2616c0d1c208
rigageno <riga.geno@yahoo.com>
2025-11-29 11:19:58 +0700

    Second Commit

---
+++ src/main/java/com/mycompany/mahasiswa_51422448/Mahasiswa_51422448.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mahasiswa_51422448;
+
+/**
+ *
+ * @author DuckyDuck
+ */
+public class Mahasiswa_51422448 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
